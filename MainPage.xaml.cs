4a01822e3685ed3a49e7ce65a90990607aeef297
deniseS85 <denisabel78@hotmail.de>
2024-10-05 12:27:59 +0200

    create own popup by error

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,40 +5,35 @@ using System.Diagnostics; // für Debug.WriteLine()
 using System.Linq;
 using System.Text.RegularExpressions;
 
-public partial class MainPage : ContentPage
-{
+public partial class MainPage : ContentPage {
     [GeneratedRegex(@"^\d*$")]
     private static partial Regex MyRegex();
 	private readonly CustomerService customerService;
     private string? selectedCustomerName;
     private Customer? newCustomer;
+    private bool isCreatingInvoice = false;
 
-	public MainPage()
-	{
+	public MainPage() {
 		InitializeComponent();
 		customerService = new CustomerService();
+        CustomPopup.PopupClosed += OnPopupClosed;
 	}
 
-	private void OnCustomerButtonClicked(object sender, EventArgs e)
-    {
+	private void OnCustomerButtonClicked(object sender, EventArgs e) {
         Button clickedButton = (Button)sender;
         string customerName = clickedButton.Text;
 
         // beim Doppelklick eines Buttons wird es wieder lila
-        if (selectedCustomerName == customerName)
-        {
-            foreach (var button in ButtonContainer.Children.OfType<Button>())
-            {
+        if (selectedCustomerName == customerName) {
+            foreach (var button in ButtonContainer.Children.OfType<Button>()) {
                 button.BackgroundColor = Color.FromArgb("#ac99ea");
             }
             selectedCustomerName = null;
             return;
         }
         // beim Auswählen eines Buttons, werden die anderen grau
-        foreach (var button in ButtonContainer.Children.OfType<Button>())
-        {
-            if (button != clickedButton)
-            {
+        foreach (var button in ButtonContainer.Children.OfType<Button>()) {
+            if (button != clickedButton) {
                 button.BackgroundColor = Color.FromArgb("#50FFFFFF");
             }
         }
@@ -48,116 +43,106 @@ public partial class MainPage : ContentPage
         var selectedCustomer = customerService.GetCustomers()
             .FirstOrDefault(c => c.Name != null && c.Name.StartsWith(customerName));
 
-        if (selectedCustomer != null)
-        {
-            if (selectedCustomerName != null && selectedCustomerName != customerName)
-            {
+        if (selectedCustomer != null) {
+            if (selectedCustomerName != null && selectedCustomerName != customerName) {
                 var previousButton = GetButtonByCustomerName(selectedCustomerName);
-                if (previousButton != null)
-                {
+                if (previousButton != null) {
                     previousButton.BackgroundColor = Color.FromArgb("#50FFFFFF");
                 }
             }
             selectedCustomerName = customerName;
         }
-        else
-        {
+        else {
             OnAddNewAddress(sender, e);
         }
     }
     
-    private Button? GetButtonByCustomerName(string customerName)
-    {
-        return customerName switch
-        {
+    private Button? GetButtonByCustomerName(string customerName) {
+        return customerName 
+        switch {
             "INMEDIA STUDIOS" => customer1Button,
             "Notario" => customer2Button,
             _ => null,
         };
     }
     
-    private async void OnAddNewAddress(object sender, EventArgs e)
-    {
+    private async void OnAddNewAddress(object sender, EventArgs e) {
         var addNewAdress = new NewAdressForm(this);
         await Navigation.PushModalAsync(addNewAdress);
     }
 
-    public void AddNewCustomer(Customer? customer)
-    {
-        if (customer != null)
-        {
+    public void AddNewCustomer(Customer? customer) {
+        if (customer != null) {
             newCustomer = customer;
             newCustomerButton.Text = newCustomer.Name;
-            OnCreateInvoice(this, EventArgs.Empty);
         }
     }
 
-	private void OnCreateInvoice(object sender, EventArgs e)
-    {
+	private async void OnCreateInvoice(object sender, EventArgs e) {
+        isCreatingInvoice = true;
+
         Customer? selectedCustomer = newCustomer ?? customerService.GetCustomers()
         .FirstOrDefault(c => c.Name != null && selectedCustomerName != null && c.Name.StartsWith(selectedCustomerName, StringComparison.OrdinalIgnoreCase));
 
-        if (selectedCustomer != null)
-        {
-            string address = $"{selectedCustomer.Name}\n{selectedCustomer.Address}\n{selectedCustomer.PostalCode} {selectedCustomer.City}\nCIF {selectedCustomer.CIF}";
-            string wholeNumberText = WholeNumberEntry.Text ?? "0";
-            string decimalText = string.IsNullOrEmpty(DecimalEntry.Text) ? "00" : DecimalEntry.Text;
-
-            if (decimal.TryParse(wholeNumberText, out decimal wholeNumber) && decimal.TryParse(decimalText, out decimal decimalPart))
-            {
-                decimal totalValue = wholeNumber + (decimalPart / 100);
-
-                if (totalValue == 0)
-                {
-                    DisplayAlert("Fehler", "Gebe eine Rechnungssumme ein.", "OK");
-                    return;
-                }
-
-                string total = $"{totalValue:C}";
-
-                ShowInvoicePDF(address, total);
-                ResetForm();
-            }
+        if (selectedCustomer == null) {
+            await Overlay.FadeTo(0.6, 100);
+            Overlay.IsVisible = true;
+            await CustomPopup.ShowPopup("Fehler", "Bitte wähle eine Adresse aus.");
+            return;
         }
-        else
-        {
-            DisplayAlert("Fehler", "Bitte wähle eine Adresse aus.", "OK");
+
+        string address = $"{selectedCustomer.Name}\n{selectedCustomer.Address}\n{selectedCustomer.PostalCode} {selectedCustomer.City}\nCIF {selectedCustomer.CIF}";
+        string wholeNumberText = string.IsNullOrEmpty(WholeNumberEntry.Text) ? "0" : WholeNumberEntry.Text;
+        string decimalText = string.IsNullOrEmpty(DecimalEntry.Text) ? "00" : DecimalEntry.Text;
+
+        if (decimal.TryParse(wholeNumberText, out decimal wholeNumber) && 
+            decimal.TryParse(decimalText, out decimal decimalPart)) {
+            decimal totalValue = wholeNumber + (decimalPart / 100);
+
+            if (isCreatingInvoice && totalValue == 0) {
+                await Overlay.FadeTo(0.6, 100);
+                Overlay.IsVisible = true;
+                await CustomPopup.ShowPopup("Fehler", "Gebe eine Rechnungssumme ein.");
+                return;                
+            }
+
+            string total = $"{totalValue:C}";
+            ShowInvoicePDF(address, total);
+            ResetForm();
+            isCreatingInvoice = false;
         }
     }
-
-
-    private void OnlyNumberValid(object sender, TextChangedEventArgs e)
-    {
-        if (sender is Entry entry)
-        {
-            if (!string.IsNullOrWhiteSpace(e.NewTextValue) && !MyRegex().IsMatch(e.NewTextValue))
-            {
+    private void OnlyNumberValid(object sender, TextChangedEventArgs e) {
+        if (sender is Entry entry) {
+            if (!string.IsNullOrWhiteSpace(e.NewTextValue) && !MyRegex().IsMatch(e.NewTextValue)) {
                 entry.Text = e.OldTextValue;
             }
         }
     }
 
-    private async void ShowInvoicePDF(string address, string total)
-    {
+    private async void ShowInvoicePDF(string address, string total) {
         var invoicePDF = new InvoicePDF(this, address, total);
         invoicePDF.OnGeneratePdfClicked(this, EventArgs.Empty);
         await Navigation.PushModalAsync(invoicePDF);
     }
 
-    public void ResetForm()
-    {
-        WholeNumberEntry.Text = string.Empty;
-        DecimalEntry.Text = string.Empty;
+    public void ResetForm() {
+        WholeNumberEntry.Text = "";
+        DecimalEntry.Text = "";
         WholeNumberEntry.Unfocus();
         DecimalEntry.Unfocus();
 
-        foreach (var button in ButtonContainer.Children.OfType<Button>())
-        {
+        foreach (var button in ButtonContainer.Children.OfType<Button>()) {
             button.BackgroundColor = Color.FromArgb("#ac99ea");
         }
         selectedCustomerName = null;
         newCustomer = null;
         newCustomerButton.Text = "Neue Adresse hinzufügen";
+    }
+
+    private async void OnPopupClosed(object? sender, EventArgs e) {
+        await Overlay.FadeTo(0, 100);
+        Overlay.IsVisible = false;
     }
 }
 
---
+++ Views/Popup.xaml.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel; // Füge dies hinzu
+
+namespace RechnungsApp.Views {
+    public partial class Popup : ContentView, INotifyPropertyChanged {
+        public event EventHandler? PopupClosed;
+        public Popup() {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+
+        private string? _title;
+        public string? Title {
+            get => _title;
+            set {
+                _title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        private string? _message;
+        public string? Message {
+            get => _message;
+            set {
+                _message = value;
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+
+        public new event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual new void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task ShowPopup(string title, string message) {
+            Title = title;
+            Message = message;
+            IsVisible = true;
+            Opacity = 0;
+            await this.FadeTo(1, 250);
+        }
+
+        public async void OnClose(object sender, EventArgs e) {
+            await this.FadeTo(0, 250);
+            IsVisible = false;
+            PopupClosed?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
+
